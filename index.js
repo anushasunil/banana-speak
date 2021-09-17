d9ad26c01391fcd988be57369cfd6470339b3b4c
Anusha <anushasunil71201@gmail.com>
2021-09-17 12:01:48 +0530

    fix: fetch and error handling function

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var inputText = document.querySelector("#input-box");
 var btnTranslate = document.querySelector("#translate");
-var outputText = document.querySelector("#output-box")
+var outputText = document.querySelector("#output-box");var errorMessage = document.querySelector(".error-message")
+
 
 btnTranslate.addEventListener("click", function clickHandler()
 {
     console.log(inputText.value);
-    url = "https://api.funtranslations.com/translate/minion.json?text=" + inputText;
-    
-    fetch(url)
-        .then((response) => {
-        var json = response.json;
-        outputText.innerText = json.translated;
+    // url = "https://api.funtranslations.com/translate/minion.json?text=" + inputText.value;
 
-    });
+    url = "https://api.funtranslations.com/translate/shakespeare.json?text=" + inputText.value
+
+    fetch(url)
+    .then((response) =>
+        response.json())
+    .then(function outputTextHandler(json){
+        console.log(json);
+        outputText.innerText = json.contents.translated;
+    })
+    .catch(errorHandling())
 });
+
+function errorHandling(error)
+{
+    errorMessage.innerText = "server frown 😣";
+}
