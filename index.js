ba779e342dad9551d34c5e15fe87ec3b9af3094d
Anusha <anushasunil71201@gmail.com>
2021-09-17 10:58:03 +0530

    docs: function and API

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 var inputText = document.querySelector("#input-box");
+var btnTranslate = document.querySelector("#translate");
+var outputText = document.querySelector("#output-box")
+
+btnTranslate.addEventListener("click", function clickHandler()
+{
+    console.log(inputText.value);
+    url = "https://api.funtranslations.com/translate/minion.json?text=" + inputText;
+    
+    fetch(url)
+        .then((response) => {
+        var json = response.json;
+        outputText.innerText = json.translated;
+
+    });
+});
